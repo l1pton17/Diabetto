80bbc96d4d9dff578583cc9d5fa45a3ad89abdf2
l1pton17 <sitdikov.arthur@gmail.com>
2019-10-26 15:50:38 +0100

    build intent

---
+++ DiabettoIntentstaticLib/build/DiabettoIntentstaticLib.build/Release-iphoneos/DiabettoIntentStaticLib.build/Objects-normal/arm64/AddMeasureIntent.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/admin/Documents/Diabetto/DiabettoIntentstaticLib/DiabettoIntentStaticLib/AddMeasureIntent.m \
+  /Users/admin/Documents/Diabetto/DiabettoIntentstaticLib/DiabettoIntentStaticLib/AddMeasureIntent.h
